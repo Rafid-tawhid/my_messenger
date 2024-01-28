441905351ac0782ece59fc4ccf5c3ddc3115602f
rafid <rafidtawhid@gmail.com>
2024-01-29 00:02:52 +0600

    trying

--- lib/pages/chat_gpt_page.dart
+++ lib/pages/chat_gpt_page.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:chat_bubbles/bubbles/bubble_normal.dart';
 import 'package:chat_gpt_flutter/chat_gpt_flutter.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 
 
@@ -19,163 +22,167 @@ class TranslateScreen extends StatefulWidget {
 
 class _TranslateScreenState extends State<TranslateScreen> {
   /// text controller
-  final chatGpt =
-      ChatGpt(apiKey: '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');
+  final String apiKey = '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';
+  final String apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
 
-  String testPrompt =
-      'Which Disney character famously leaves a glass slipper behind at a royal ball?';
-  var testRequest;
-  List<dynamic> _message=[];
-  StreamSubscription? _subscription;
-  TextEditingController controller=TextEditingController();
-
-  void sendMessage(){
-    ChatGpt chatGpt;
-    ChatMessage(user: ChatUser(id: '121'), createdAt: DateTime.now());
-    
-    final request=CompleteText(
-      model: KeyDataTransitMode.keyDataThenRawKeyData.name,
-      maxTokens: 200,
-      prompt: controller.text,);
-
-
-  }
-
-  @override
-  void initState() {
-
-    super.initState();
-  }
+  TextEditingController controller = TextEditingController();
+  ScrollController scrollController = ScrollController();
+  List<Message> msgs = [];
+  bool isTyping = false;
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Flexible(
+      appBar: AppBar(
+        title: Text('ChatGPT App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
               child: ListView.builder(
-                itemCount: _message.length,
-                itemBuilder: (context,index)=>_message[index],
-              )),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+                  controller: scrollController,
+                  itemCount: msgs.length,
+                  shrinkWrap: true,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: isTyping && index == 0
+                            ? Column(
+                          children: [
+                            BubbleNormal(
+                              text: msgs[0].msg,
+                              isSender: true,
+                              color: Colors.blue.shade100,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 16, top: 4),
+                              child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text("Typing...")),
+                            )
+                          ],
+                        )
+                            : BubbleNormal(
+                          text: msgs[index].msg,
+                          isSender: msgs[index].isSender,
+                          color: msgs[index].isSender
+                              ? Colors.blue.shade100
+                              : Colors.grey.shade200,
+                        ));
+                  }),
+            ),
+
+            Row(
               children: [
                 Expanded(
-                    child: TextField(
-                      controller: controller,
-                  decoration: InputDecoration(hintText: 'Send Message'),
-                )),
-                IconButton(onPressed: sendMessage, icon: Icon(Icons.send))
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: TextField(
+                          controller: controller,
+                          textCapitalization: TextCapitalization.sentences,
+                          onSubmitted: (value) {
+                            sendMsg();
+                          },
+                          textInputAction: TextInputAction.send,
+                          showCursor: true,
+                          decoration: const InputDecoration(
+                              border: InputBorder.none, hintText: "Enter text"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    sendMsg();
+                  },
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                )
               ],
             ),
-          )
-        ],
+
+          ],
+        ),
       ),
     );
   }
+
+  void sendMsg() async {
+    String text = controller.text;
+    String apiKey = "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";
+    controller.clear();
+    try {
+      if (text.isNotEmpty) {
+        setState(() {
+          msgs.insert(0, Message(true, text));
+          isTyping = true;
+        });
+        scrollController.animateTo(0.0,
+            duration: const Duration(seconds: 1), curve: Curves.easeOut);
+        var response = await http.post(
+            Uri.parse("https://api.openai.com/v1/chat/completions"),
+            headers: {
+              "Authorization": "Bearer $apiKey",
+              "Content-Type": "application/json"
+            },
+            body: jsonEncode({
+              "model": "gpt-3.5-turbo",
+              "messages": [
+                {"role": "user", "content": text}
+              ]
+            }));
+        print('response ${response.body.toString()}');
+        if (response.statusCode == 200) {
+          var json = jsonDecode(response.body);
+          setState(() {
+            isTyping = false;
+            msgs.insert(
+                0,
+                Message(
+                    false,
+                    json["choices"][0]["message"]["content"]
+                        .toString()
+                        .trimLeft()));
+          });
+          scrollController.animateTo(0.0,
+              duration: const Duration(seconds: 1), curve: Curves.easeOut);
+        }
+      }
+    } on Exception {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Some error occurred, please try again!")));
+    }
+  }
+
 }
 
-// class ChatGptPage extends StatefulWidget {
-//   static const String routeName='/chat_gpt';
-//   const ChatGptPage({super.key});
-//
-//   @override
-//   State<ChatGptPage> createState() => _ChatGptPageState();
-// }
-//
-// class _ChatGptPageState extends State<ChatGptPage> {
-//   final _openAI = OpenAI.instance.build(
-//     token: OPENAI_API_KEY,
-//     baseOption: HttpSetup(
-//       receiveTimeout: const Duration(
-//         seconds: 5,
-//       ),
-//     ),
-//     enableLog: true,
-//   );
-//
-//   final ChatUser _currentUser =
-//   ChatUser(id: '1', firstName: 'Rafid', lastName: 'Tawhid');
-//
-//   final ChatUser _gptChatUser =
-//   ChatUser(id: '2', firstName: 'Virtual', lastName: 'Assistant');
-//
-//   List<ChatMessage> _messages = <ChatMessage>[];
-//   List<ChatUser> _typingUsers = <ChatUser>[];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: const Color.fromRGBO(
-//           0,
-//           166,
-//           126,
-//           1,
-//         ),
-//         title: const Text(
-//           'Virtual Assistant',
-//           style: TextStyle(
-//             color: Colors.white,
-//           ),
-//         ),
-//       ),
-//       body: DashChat(
-//           currentUser: _currentUser,
-//           typingUsers: _typingUsers,
-//           messageOptions: const MessageOptions(
-//             currentUserContainerColor: Colors.black,
-//             containerColor: Color.fromRGBO(
-//               0,
-//               166,
-//               126,
-//               1,
-//             ),
-//             textColor: Colors.white,
-//           ),
-//           onSend: (ChatMessage m) {
-//             getChatResponse(m);
-//           },
-//           messages: _messages),
-//     );
-//   }
-//
-//   Future<void> getChatResponse(ChatMessage m) async {
-//     setState(() {
-//       _messages.insert(0, m);
-//       _typingUsers.add(_gptChatUser);
-//     });
-//     List<Messages> _messagesHistory = _messages.reversed.map((m) {
-//       if (m.user == _currentUser) {
-//         return Messages(role: Role.user, content: m.text);
-//       } else {
-//         return Messages(role: Role.assistant, content: m.text);
-//       }
-//     }).toList();
-//     final request = ChatCompleteText(
-//       model: GptTurbo0301ChatModel(),
-//       messages: _messagesHistory,
-//       maxToken: 200,
-//     );
-//     final response = await _openAI.onChatCompletion(request: request);
-//     for (var element in response!.choices) {
-//       if (element.message != null) {
-//         setState(() {
-//           _messages.insert(
-//             0,
-//             ChatMessage(
-//                 user: _gptChatUser,
-//                 createdAt: DateTime.now(),
-//                 text: element.message!.content),
-//           );
-//         });
-//       }
-//     }
-//     setState(() {
-//       _typingUsers.remove(_gptChatUser);
-//     });
-//   }
-// }
+
+class Message {
+  bool isSender;
+  String msg;
+  Message(this.isSender, this.msg);
+}
